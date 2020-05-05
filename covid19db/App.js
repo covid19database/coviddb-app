06693df803cdd7e77ef25585971a2bc5bf40115c
Alvin Wan <alvinwan1@gmail.com>
2020-05-05 16:11:07 -0700

    basic homepage

--- covid19db/App.js
+++ covid19db/App.js
@@ -1,25 +1,57 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native';
 
+
+const ButtonBackgroundless = (props) => {
+    return (
+      <TouchableOpacity
+          style={styles.buttonBackgroundless}
+          onPress={props.onPress}
+          underlayColor='#fff'>
+          <Text style={styles.buttonBackgroundlessText}>{props.text}</Text>
+      </TouchableOpacity>
+    );
+}
+
+
+/**
+ * ButtonLight
+ * ===
+ * light gray button with dark text, not full-width by default
+ *
+ * Example onPress events:
+ *
+ *    () => { alert('You tapped the button!'); }
+ *    () => navigate('HomeScreen')
+ */
+// Example onPress events
+const ButtonLight = (props) => {
+    return (
+      <TouchableOpacity
+          style={styles.buttonLight}
+          onPress={props.onPress}
+          underlayColor='#fff'>
+          <Text style={styles.buttonLightText}>{props.text}</Text>
+      </TouchableOpacity>
+    );
+}
+
+
 export default function App() {
   return (
     <View style={styles.container}>
-        <TouchableOpacity
-            style={styles.buttonLight}
+        <Text style={styles.heading}>No contact detected</Text>
+        <Text style={styles.copy}>Based on your data, you have not been nearby someone who tested positive for COVID-19.</Text>
+        <ButtonLight
             onPress={() => {
               alert('You tapped the button!');
             }}
-            // onPress={() => navigate('HomeScreen')}
-            underlayColor='#fff'>
-            <Text style={styles.buttonLightText}>Check for symptoms</Text>
-        </TouchableOpacity>
-        <Button
-          style={styles.buttonNoBgText}
-          onPress={() => {
-              alert('Nawwwwww');
-          }}
-          title="Turn off contact tracing"
-        />
+            text="Check for symptoms" />
+        <ButtonBackgroundless
+            onPress={() => {
+                alert('Nawwwwww');
+            }}
+            text="Turn off contact tracing" />
     </View>
   );
 }
@@ -42,8 +74,21 @@ const styles = StyleSheet.create({
   buttonLightText: {
     fontSize: 17,
   },
-  buttonNoBgText: {
+  buttonBackgroundlessText: {
+    paddingTop: 15,
+    paddingBottom: 15,
     fontSize: 15,
-
+    color: '#666666'
+  },
+  heading: {
+    fontWeight: 'bold',
+    fontSize: 17
+  },
+  copy: {
+    fontSize: 15,
+    paddingRight: 30,
+    paddingLeft: 30,
+    paddingTop: 10,
+    paddingBottom:20,
   }
 });
